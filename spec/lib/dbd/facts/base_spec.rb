62508d6f9bc73b022c6c1fd8cb7a9e5b0c59091d
Peter Vandenabeele <peter@vandenabeele.com>
2013-03-30 09:51:24 +0100

    Add some play tests for RDF module

--- spec/lib/dbd/facts/base_spec.rb
+++ spec/lib/dbd/facts/base_spec.rb
@@ -10,6 +10,32 @@ module Dbd
       it "The RDF module is loaded" do
         RDF # should not raise error
       end
+
+      describe "play with a graph" do
+
+        let(:graph) {RDF::Graph.new << [:hi, RDF::DC.title, "Hello, world!"]}
+
+        it "Create a graph" do
+          graph # should_not raise_error
+        end
+
+        it "writing data" do
+          graph.dump(:ntriples).chomp.should ==
+            '_:hi <http://purl.org/dc/terms/title> "Hello, world!" .'
+        end
+
+        it "Reading N-triples" do
+          n_triples_string =
+            '<http://example.com/test#1> ' +
+            '<http://www.w3.org/2000/01/rdf-schema#label> ' +
+            '"test 1" .'
+          RDF::Reader.for(:ntriples).new(n_triples_string) do |reader|
+            reader.each_statement do |statement|
+               statement.to_ntriples.chomp.should == n_triples_string
+            end
+          end
+        end
+      end
     end
   end
 end
