45976d04dc720168e6a9d84b2660766f676d9162
Peter Vandenabeele <peter@vandenabeele.com>
2013-04-07 14:46:27 +0200

    DataFact.new has a very fine grained time stamp

--- lib/dbd/facts/data_fact.rb
+++ lib/dbd/facts/data_fact.rb
@@ -2,10 +2,11 @@ module Dbd
   module Facts
     class DataFact
 
-      attr_reader :id
+      attr_reader :id, :time_stamp
 
       def initialize
         @id = Helpers::TempUUID.new
+        @time_stamp = Time.new.utc
       end
 
     end
--- spec/lib/dbd/facts/data_fact_spec.rb
+++ spec/lib/dbd/facts/data_fact_spec.rb
@@ -16,6 +16,22 @@ module Dbd
           it "has a unique id (UUID)" do
             subject.id.should be_a(Helpers::TempUUID)
           end
+
+          it "two data_facts have different id" do
+            data_fact_1 = described_class.new
+            data_fact_2 = described_class.new
+            data_fact_1.id.should_not == data_fact_2
+          end
+
+          it "has a very fine grained time stamp" do
+            subject.time_stamp.should be_a(Time)
+          end
+
+          it "the time_stamps of 2 consecutive created facts should be different" do
+            data_fact_1 = described_class.new
+            data_fact_2 = described_class.new
+            data_fact_1.time_stamp.should < data_fact_2.time_stamp
+          end
         end
       end
     end
