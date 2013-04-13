85d972201f4805af8351bafaa95fbc7d35965e7e
Peter Vandenabeele <peter@vandenabeele.com>
2013-04-13 04:20:41 +0200

    Only newer facts can be added to FacetCollection
    * adding equal or older time_stamp raises OutOfOrderError

--- lib/dbd/facts/collection.rb
+++ lib/dbd/facts/collection.rb
@@ -4,6 +4,9 @@ module Dbd
 
       include Enumerable
 
+      class OutOfOrderError < StandardError
+      end
+
       def initialize()
         @internal_collection = []
       end
@@ -14,7 +17,13 @@ module Dbd
         end
       end
 
+      def newest_time_stamp
+        newest_entry = @internal_collection.last
+        newest_entry && newest_entry.time_stamp
+      end
+
       def <<(element)
+        raise OutOfOrderError if (self.newest_time_stamp && element.time_stamp <= self.newest_time_stamp)
         @internal_collection << element
       end
 
--- spec/lib/dbd/facts/collection_spec.rb
+++ spec/lib/dbd/facts/collection_spec.rb
@@ -23,16 +23,56 @@ module Dbd
         end
 
         it "adding an element works" do
-          subject << :a
+          subject << fact_1
           subject.count.should == 1
         end
 
         it "other functions (e.g. []) do not work" do
-          subject << :a
-          subject << :b
+          subject << fact_1
+          subject << fact_2
           lambda {subject[1]} . should raise_exception NoMethodError
         end
+      end
 
+      describe "newest_time_stamp" do
+        it "returns nil for empty collection" do
+          subject.newest_time_stamp.should be_nil
+        end
+
+        it "returns a time_stamp" do
+          subject << fact_1
+          subject.newest_time_stamp.should be_a(fact_1.time_stamp.class)
+        end
+
+        it "returns the newest time_stamp" do
+          subject << fact_1
+          subject << fact_2
+          fact_1.time_stamp.should_not == fact_2.time_stamp
+          subject.newest_time_stamp.should == fact_2.time_stamp
+        end
+      end
+
+      describe "validate that only 'newer' elements are added" do
+
+        it "adding an element with a newer time_stamp succeeds" do
+          fact_1
+          fact_2
+          subject << fact_1
+          subject << fact_2
+        end
+
+        it "adding an element with an older time_stamp fails" do
+          fact_1
+          fact_2
+          subject << fact_2
+          lambda { subject << fact_1 } . should raise_error(Collection::OutOfOrderError)
+        end
+
+        it "adding an element with ani equal time_stamp fails" do
+          fact_1
+          subject << fact_1
+          lambda { subject << fact_1 } . should raise_error(Collection::OutOfOrderError)
+        end
       end
     end
   end
