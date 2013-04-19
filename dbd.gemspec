a892eb6bf0d32a5577f64fa1dc92f7476856094b
Peter Vandenabeele <peter@vandenabeele.com>
2013-04-20 00:42:45 +0200

    Add first YARD documentation

--- dbd.gemspec
+++ dbd.gemspec
@@ -23,6 +23,7 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'guard-rspec'
   spec.add_development_dependency 'rb-fsevent', '~> 0.9'
   spec.add_development_dependency 'terminal-notifier-guard'
+  spec.add_development_dependency 'yard'
   spec.add_runtime_dependency 'neography'
   spec.add_runtime_dependency 'ruby_peter_v'
   spec.add_runtime_dependency 'uuidtools'
--- lib/dbd/graph/to_CSV.rb
+++ lib/dbd/graph/to_CSV.rb
@@ -2,12 +2,20 @@ module Dbd
   module Graph
     module ToCSV
 
+      # Export a graph to a CSV
+      #
+      # @param (none)
+      #
+      # @return a comma separated CSV with double quoted strings
+      #
+      # @api public
       def to_CSV
         self.map do |e|
           e.map do
             "\"abc-def-ghi\",\"blah foo bar\",\"2013-05-01 13:36:45.456893045\""
           end
         end.flatten.join("\n").encode("utf-8") + "\n"
+        # see rejected bug http://bugs.ruby-lang.org/issues/8289
       end
 
     end
