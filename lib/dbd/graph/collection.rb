4740d8b17aec4e641265867c69d8b070207221ce
Peter Vandenabeele <peter@vandenabeele.com>
2013-04-27 14:58:17 +0200

    Graph::Collection#newest_time_stamp

--- lib/dbd/graph/collection.rb
+++ lib/dbd/graph/collection.rb
@@ -4,6 +4,12 @@ module Dbd
 
       include Helpers::ArrayCollection
 
+      def newest_time_stamp
+        # TODO filter only fact_collections !!
+        newest_entry = @internal_collection.last
+        newest_entry && newest_entry.newest_time_stamp
+      end
+
     end
   end
 end
--- spec/lib/dbd/graph/collection_spec.rb
+++ spec/lib/dbd/graph/collection_spec.rb
@@ -4,9 +4,10 @@ module Dbd
   module Graph
     describe Collection do
 
-      let(:fact_origin_collection_1) {Factories::FactOrigin::Collection.me_tijd}
-      let(:fact_collection_1_2) {Factories::Fact::Collection.fact_1_2}
-      let(:fact_collection_3_4) {Factories::Fact::Collection.fact_3_4}
+      let(:fact_origin_collection_1) { Factories::FactOrigin::Collection.me_tijd }
+      let(:fact_1) { Factories::Fact.fact_1 }
+      let(:fact_collection_1_2) { Factories::Fact::Collection.fact_1_2 }
+      let(:fact_collection_3_4) { Factories::Fact::Collection.fact_3_4 }
 
       describe "create a graph_collection" do
         it "new does not fail" do
@@ -35,6 +36,24 @@ module Dbd
           subject.count.should == 2
         end
       end
+
+      describe "newest_time_stamp" do
+        it "returns nil for empty collection" do
+          subject.newest_time_stamp.should be_nil
+        end
+
+        it "returns a time_stamp" do
+          subject << fact_collection_1_2
+          subject.newest_time_stamp.should be_a(fact_1.time_stamp.class)
+        end
+
+        it "returns the newest time_stamp" do
+          subject << fact_collection_1_2
+          subject << fact_collection_3_4
+          subject.newest_time_stamp.should == fact_collection_3_4.last.time_stamp
+        end
+      end
+
     end
   end
 end
