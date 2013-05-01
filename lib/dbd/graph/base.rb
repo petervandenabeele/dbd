3b06ae1fe5b567f83258c5e4e1d5d87a7a8b3352
Peter Vandenabeele <peter@vandenabeele.com>
2013-05-01 23:18:04 +0200

    Export the fact collection in a graph to a CSV

--- lib/dbd/graph/base.rb
+++ lib/dbd/graph/base.rb
@@ -4,13 +4,14 @@ module Dbd
   module Graph
     class Base
 
-      attr_reader :fact_origin_collections
+      attr_reader :fact_origin_collections, :fact_collections
 
       def initialize
         @fact_origin_collections = Collection.new
+        @fact_collections = Collection.new
       end
 
-      # Export a graph to a CSV
+      # Export the fact_origin collection in a graph to a CSV
       #
       # @param (none)
       #
@@ -22,6 +23,23 @@ module Dbd
           self.fact_origin_collections.each do |fact_origin_collection|
             fact_origin_collection.each do |id, fact_origin|
               csv << fact_origin.values
+            end
+          end
+        end.encode("utf-8")
+      end
+
+      # Export the fact collection in a graph to a CSV
+      #
+      # @param (none)
+      #
+      # @return a comma separated CSV with double quoted strings
+      #
+      # @api public
+      def to_fact_CSV
+        CSV.generate(force_quotes: true) do |csv|
+          self.fact_collections.each do |fact_collection|
+            fact_collection.each do |fact|
+              csv << fact.values
             end
           end
         end.encode("utf-8")
--- spec/lib/dbd/graph/base_spec.rb
+++ spec/lib/dbd/graph/base_spec.rb
@@ -6,6 +6,8 @@ module Dbd
 
       let(:fact_origin_collection_1) { Factories::FactOrigin::Collection.me_tijd }
       let(:fact_origin_collection_special) { Factories::FactOrigin::Collection.special }
+      let(:fact_origin_1) { Factories::FactOrigin.me }
+      let(:data_fact_collection_1_2) { Factories::DataFact::Collection.data_fact_1_2(fact_origin_1.id) }
 
       describe "create a graph" do
         it "does not fail" do
@@ -90,6 +92,82 @@ module Dbd
 
           it "has valid_until as 10th value" do
             first_line.split(',')[9].should match(/"\d{4}-\d\d-\d\d \d\d:\d\d:\d\d UTC"/)
+          end
+        end
+
+        describe "handles comma, double quote and newline correctly" do
+
+          before do
+            subject.fact_origin_collections << fact_origin_collection_special
+          end
+
+          it "has original_source with special characters and double quote escaped" do
+            subject.to_fact_origin_CSV.should match(/"this has a comma , a newline \n and a double quote """/)
+          end
+        end
+      end
+
+      describe "#to_fact_CSV" do
+        it "returns a string" do
+          subject.to_fact_CSV.should be_a(String)
+        end
+
+        it "returns a string in UTF-8 encoding" do
+          subject.to_fact_CSV.encoding.should == Encoding::UTF_8
+        end
+
+        it "returns a string with comma's" do
+          subject.fact_collections << data_fact_collection_1_2
+          subject.to_fact_CSV.should match(/\A"[^",]+","[^",]+","[^",]+"/)
+        end
+
+        describe "with a single fact collection" do
+
+          before do
+            subject.fact_collections << data_fact_collection_1_2
+          end
+
+          it "has two lines" do
+            subject.to_fact_CSV.lines.size.should == 2
+          end
+
+          it "ends with a newline" do
+            subject.to_fact_CSV.lines.last[-1].should == "\n"
+          end
+        end
+
+        describe "has all properties of the fact_collection" do
+
+          before do
+            subject.fact_collections << data_fact_collection_1_2
+          end
+
+          let(:first_line) do
+            subject.to_fact_CSV.lines.to_a.first.chomp
+          end
+
+          it "has id (a uuid) as first value" do
+            first_line.split(',')[0].should match(/"[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}"/)
+          end
+
+          it "has time_stamp as second value" do
+            first_line.split(',')[1].should match(/"\d{4}-\d\d-\d\d \d\d:\d\d:\d\d UTC"/)
+          end
+
+          it "has fact_origin_1.id as third value" do
+            first_line.split(',')[2].should == "\"#{fact_origin_1.id.to_s}\""
+          end
+
+          it "has subject_id as 4th value" do
+            first_line.split(',')[3].should match(/"[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}"/)
+          end
+
+          it "has data_property as 5th value" do
+            first_line.split(',')[4].should == '"http://example.org/test/name"'
+          end
+
+          it "has object as 6th value" do
+            first_line.split(',')[5].should == '"The great gatzbe"'
           end
         end
 
