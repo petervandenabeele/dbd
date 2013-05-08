4153e1ea19efd5a488ea4b0f4adea143d989c57d
Peter Vandenabeele <peter@vandenabeele.com>
2013-05-09 00:43:26 +0200

    Move Graph::Base to Graph
    * removed the unused Graph::Collection class
      (Graph now uses 1 Fact::Collection, that stores
       both facts and provenance_facts)
    * later the graph collection may be readded when
      we really need to store multiple files
      (but probably YAGNI by then ...)
    * this further cleans up to public API (as
      seen in YARD):
      graph = Graph.new
      graph.store_fact_set # TBD
      graph.to_CSV
      graph.from_CSV(IO) # TBD

--- lib/dbd/graph.rb
+++ lib/dbd/graph.rb
@@ -1,2 +1,23 @@
-require 'dbd/graph/base.rb'
-require 'dbd/graph/collection.rb'
+require 'csv'
+
+module Dbd
+  class Graph
+
+    def initialize
+      @fact_collection = Fact::Collection.new
+    end
+
+    ##
+    # Export the graph to a CSV string
+    #
+    # @return [String] comma separated string with double quoted cells
+    def to_CSV
+      CSV.generate(force_quotes: true) do |csv|
+        @fact_collection.each do |fact|
+          csv << fact.values
+        end
+      end.encode("utf-8")
+    end
+
+  end
+end
