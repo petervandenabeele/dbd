13b41e0368000455bbbea0a19e606e98dafeccdc
Peter Vandenabeele <peter@vandenabeele.com>
2013-05-12 13:38:24 +0200

    Remove ResourcesWithProvenance (too complicated for now)

--- lib/dbd.rb
+++ lib/dbd.rb
@@ -6,7 +6,6 @@ require 'dbd/fact'
 require 'dbd/provenance_fact'
 require 'dbd/resource'
 require 'dbd/provenance_resource'
-require 'dbd/resources_with_provenance'
 require 'dbd/graph'
 
 require 'dbd/rdf'
