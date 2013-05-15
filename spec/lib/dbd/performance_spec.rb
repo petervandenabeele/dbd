134f5155820202e3cc4b95238a61a59a6786a93c
Peter Vandenabeele <peter@vandenabeele.com>
2013-05-15 22:48:24 +0200

    Improve the performance_spec
    * Inspired by Benchmark
    * use a rehearsal step
    * use a larger number of iterations (10K instead of 1K)
    * now jruby (58 us) is only lightly slower then MRI (38 us),
      which seems more reasonable
    * with larger amounts of iterations (100K), JRuby becomes faster
      (48 us) and MRI ruby becomes slower (58 us), probably because
      Garbage Collection kicks in.

--- spec/lib/dbd/performance_spec.rb
+++ spec/lib/dbd/performance_spec.rb
@@ -1,7 +1,10 @@
 require 'spec_helper'
+require 'benchmark'
 
 module Dbd
   describe "performance" do
+
+    include Benchmark
 
     def new_subject
       Fact.new_subject
@@ -9,29 +12,29 @@ module Dbd
 
     let(:provenance_fact_1) { Factories::ProvenanceFact.context(new_subject) }
 
-    NUMBER_OF_FACTS = 1000
+    NUMBER_OF_FACTS = 10_000
 
-    describe "1000 facts" do
+    describe "#{NUMBER_OF_FACTS} facts" do
       it "reports and checks the used time" do
         graph = Graph.new
         graph << provenance_fact_1
+        # Rehearsal
+        NUMBER_OF_FACTS.times do |counter|
+          data_fact = Factories::Fact.data_fact(provenance_fact_1, new_subject)
+          graph << data_fact
+        end
+        # Actual
         start = Time.now
         NUMBER_OF_FACTS.times do |counter|
           data_fact = Factories::Fact.data_fact(provenance_fact_1, new_subject)
           graph << data_fact
         end
         duration = Time.now - start
-        puts "\nDuration for inserting #{NUMBER_OF_FACTS} facts in the in-memory graph was #{duration*1000} ms"
-        graph.size.should == NUMBER_OF_FACTS + 1
-        duration.should <
-          # dramatic performance difference, not immediately found what is the cause
-          # creating new data_fact objects consumes 330 ms of the typ. 400 ms on JRuby
-          case RUBY_ENGINE
-          when "ruby"
-            0.100 # 100 ms (typ. 30 ms on Mac Ruby 2.0.0)
-          when "jruby"
-            1.000 # 1000 ms (typ. 400 ms on Mac jruby 1.7.3)
-          end
+        puts "\nDuration for inserting #{NUMBER_OF_FACTS} facts in the in-memory graph was #{duration*1000_000/NUMBER_OF_FACTS} us PER FACT"
+        graph.size.should == 2 * NUMBER_OF_FACTS + 1
+        duration.should < 0.000_1 * NUMBER_OF_FACTS
+        # typ. 37 us on Mac Ruby 2.0.0
+        # typ. 60 us on Mac jruby 1.7.3
       end
     end
   end
